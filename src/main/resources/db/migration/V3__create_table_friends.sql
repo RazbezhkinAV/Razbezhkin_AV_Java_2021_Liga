a17b51653d5aa05ce85d4e5219f77c6007d30c85
Razbezhkin_Aleksandr <Chevik93@icloud.com>
2021-10-07 12:15:40 +0300

    update solution lesson 7

--- src/main/resources/db/migration/V3__create_table_friends.sql
+++ src/main/resources/db/migration/V3__create_table_friends.sql
@@ -2,5 +2,6 @@ create table friends(
     person_id integer,
     friends_id integer,
     foreign key (person_id) references person (id) on delete cascade,
-    foreign  key (friends_id) references person (id)
+    foreign  key (friends_id) references person (id),
+    primary key(person_id,friends_id)
 );
